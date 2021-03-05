e0da623c54b9eca0eed1dce131dd9f1e1fe95715
Filip Sunnemar <filipsunnemar@hotmail.com>
2021-03-05 09:52:24 +0100

    divide date

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -3,18 +3,32 @@
     <div class="content">
       <template v-if="filteredPosts.length === 0">
         <div class="posts" v-for="(post, index) in posts" v-bind:key="index">
-          <h5>{{ post.date }}</h5>
-          <h3 class="title" >{{ post.title.rendered }}</h3>
-          <p>{{post.content.rendered}} </p>
-          
+          <div class="date">
+            <h5>{{(post.date.slice(0,10))}}</h5>
+            <h5>{{(post.date.slice(11,21))}}</h5>
+
+          </div>
+          <h3 class="title">{{ post.title.rendered }}</h3>
+          <p>
+            <span class="htmlContent" v-html="post.excerpt.rendered"></span>
+          </p>
         </div>
       </template>
       <template v-else>
-        <div class="posts" v-for="(post, index) in filteredPosts" v-bind:key="index">
-          <h5>{{ post.date }}</h5>
+        <div
+          class="posts"
+          v-for="(post, index) in filteredPosts"
+          v-bind:key="index"
+        >
+           <div class="date">
+            <h5>{{(post.date.slice(0,10))}}</h5>
+            <h5>{{(post.date.slice(11,21))}}</h5>
+
+          </div>
           <h3 class="title">{{ post.title.rendered }}</h3>
-          <p>{{post.content.rendered}} </p>
-          
+          <p>
+            <span class="htmlContent" v-html="post.excerpt.rendered"></span>
+          </p>
         </div>
       </template>
     </div>
@@ -39,21 +53,27 @@ export default {
   display: flex;
   flex-direction: column;
   padding: 1rem;
-  border: 0.5px solid black;
-  width: clamp(35ch, 50%, 70ch)
-  
+  box-shadow: 0 0 3pt 2pt black;
+  width: clamp(35ch, 50%, 90ch);
 }
 
 .posts {
-  margin: 1rem 0 1rem 0
+  margin: 1rem 0 1rem 0;
 }
 
 .title {
   margin: 1rem 0 1rem 0;
-  font-size: clamp(1rem, 2.2vw, 3rem); 
+  font-size: clamp(1rem, 2.2vw, 3rem);
 }
 
 p {
-  font-size: clamp(1rem, 1.6vw, 3rem); 
+  font-size: clamp(1rem, 1.6vw, 3rem);
+}
+.date {
+  display: flex;
+}
+
+h5 {
+  margin-right: 1rem;
 }
 </style>
